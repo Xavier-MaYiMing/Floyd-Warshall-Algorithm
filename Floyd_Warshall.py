4a8208308509209bc41cb4da219c46d18a698f9f
Yi-Ming Ma (Xavier Ma) <108516554+Xavier-MaYiMing@users.noreply.github.com>
2022-12-05 19:55:33 +0800

    Add files via upload

---
+++ Floyd_Warshall.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/12/4 21:46
+# @Author  : Xavier Ma
+# @Email   : xavier_mayiming@163.com
+# @File    : Floyd_Warshall.py
+# @Statement : The Floyd-Warshall algorithm for the shortest path problem
+# @Reference : Floyd R W. Algorithm 97: shortest path[J]. Communications of the ACM, 1962, 5(6): 345.
+
+
+def main(network):
+    """
+    The main function of the Floyd-Warshall algorithm
+    :param network: {node1: {node2: length, node3: length, ...}, ...}
+    :return:
+    """
+    # Step 1. Initialization
+    nn = len(network)  # node number
+    inf = float('inf')
+    dis_mat = [[inf for _ in range(nn)] for _ in range(nn)]
+    path_mat = [[[] for _ in range(nn)] for _ in range(nn)]
+    for node1 in range(nn):
+        dis_mat[node1][node1] = 0
+        path_mat[node1][node1] = [node1]
+        for node2 in network[node1].keys():
+            dis_mat[node1][node2] = network[node1][node2]
+            path_mat[node1][node2] = [node1, node2]
+
+    # Step 2. The main loop:
+    for k in range(nn):
+        for i in range(nn):
+            for j in range(nn):
+                if dis_mat[i][j] > dis_mat[i][k] + dis_mat[k][j]:
+                    dis_mat[i][j] = dis_mat[i][k] + dis_mat[k][i]
+                    path_mat[i][j] = path_mat[i][k].copy()
+                    path_mat[i][j].pop()
+                    path_mat[i][j].extend(path_mat[k][j])
+    return {'path': path_mat, 'length': dis_mat}
+
+
+if __name__ == '__main__':
+    test_network = {
+        0: {1: 62, 2: 44, 3: 67},
+        1: {0: 62, 2: 32, 4: 52},
+        2: {0: 44, 1: 33, 3: 32, 4: 52},
+        3: {0: 67, 2: 32, 4: 54},
+        4: {1: 52, 2: 52, 3: 54}
+    }
+    print(main(test_network))
---
+++ README.md
@@ -0,0 +1,50 @@
+### Floyd-Warshall Algorithm
+
+##### Reference: Floyd R W. Algorithm 97: shortest path[J]. Communications of the ACM, 1962, 5(6): 345.
+
+The Floyd-Warshall Algorithm for the shortest path problem
+
+| Variables | Meaning                                                      |
+| --------- | ------------------------------------------------------------ |
+| network   | Dictionary, {node1: {node2: length, node3: length, ...}, ...} |
+| nn        | The number of nodes                                          |
+| dis_mat   | List, dis_mat\[i\]\[j\] denotes the length of the shortest path from node i to node j |
+| path_mat  | List, path_mat\[i\]\[j\] denotes the shortest path from node i to node j |
+
+#### Example
+
+![image-20220704083123627](C:\Users\dell\AppData\Roaming\Typora\typora-user-images\image-20220704083123627.png)
+
+```python
+if __name__ == '__main__':
+    test_network = {
+        0: {1: 62, 2: 44, 3: 67},
+        1: {0: 62, 2: 32, 4: 52},
+        2: {0: 44, 1: 33, 3: 32, 4: 52},
+        3: {0: 67, 2: 32, 4: 54},
+        4: {1: 52, 2: 52, 3: 54}
+    }
+    print(main(test_network))
+```
+
+##### Output
+
+```python
+{
+    'path': [
+        [[0], [0, 1], [0, 2], [0, 3], [0, 2, 4]], 
+        [[1, 0], [1], [1, 2], [1, 2, 3], [1, 4]], 
+        [[2, 0], [2, 1], [2], [2, 3], [2, 4]], 
+        [[3, 0], [3, 2, 1], [3, 2], [3], [3, 4]], 
+        [[4, 2, 0], [4, 1], [4, 2], [4, 3], [4]]
+    ], 
+    'length': [
+        [0, 62, 44, 67, 88], 
+        [62, 0, 32, 65, 52], 
+        [44, 33, 0, 32, 52], 
+        [67, 64, 32, 0, 54], 
+        [104, 52, 52, 54, 0]
+    ]
+}
+```
+
